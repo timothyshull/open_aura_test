04777ed9de5be3f6f3fbfc3bff1fa89e72234459
timothyshull <timothyshull@gmail.com>
2015-03-25 16:55:09 -0700

    Add controller for base view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'basic_pages/home'
+
+  get 'basic_page/home'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
